94ba17ddeef883fa7233ea0a03220ec7ef1f8c39
Łukasz Ciura <lciura@future-processing.com>
2019-11-19 11:50:36 +0100

    Draft of backend solution in .NET Core 3.0

---
+++ backend/MetricsDashboard.WebApi/Controllers/MetricsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MetricsDashboard.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MetricsController : ControllerBase
+    {
+    }
+}
---
+++ backend/MetricsDashboard.WebApi/DependencyModules/ApiModule.cs
@@ -0,0 +1,8 @@
+﻿using Autofac;
+
+namespace MetricsDashboard.WebApi.DependencyModules
+{
+    public class ApiModule : Module
+    {
+    }
+}
