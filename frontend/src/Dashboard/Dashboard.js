dc2059abfa823704843fd5e5ea8f9291e7aa441f
Łukasz Ciura <lciura@future-processing.com>
2019-11-21 17:36:27 +0100

    Added postman collection

--- frontend/src/Dashboard/Dashboard.js
+++ frontend/src/Dashboard/Dashboard.js
@@ -4,36 +4,60 @@ import Box from "@material-ui/core/Box";
 import MetricTile from "./MetricTile";
 import Typography from "@material-ui/core/Typography";
 import "./styles.css";
+import config from "../config";
 
-export default function Dashboard() {
-  return (
-    <div className="main">
-      <Typography textAlign="center" variant="h2" color="primary">
-        <Box lineHeight={2} textAlign="center">
-          Metrics
-        </Box>
-      </Typography>
-      <Grid
-        container
-        direction="row"
-        justify="center"
-        alignItems="flex-start"
-        className="dashboard-grid"
-        spacing={4}
-      >
-        <Grid item>
-          <MetricTile name={"BE Unit Test Coverate"} />
+class Dashboard extends React.Component {
+  componentDidMount() {
+    this.fetchMetrics();
+  }
+
+  fetchMetrics() {
+    fetch(config.api.URL + `/metrics/`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ metrics: [...data] });
+        console.log(data);
+      });
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <Typography textAlign="center" variant="h2" color="primary">
+          <Box lineHeight={2} textAlign="center">
+            Metrics
+          </Box>
+        </Typography>
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="flex-start"
+          className="dashboard-grid"
+          spacing={4}
+        >
+          <Grid item>
+            <MetricTile name={"BE Unit Test Coverate"} />
+          </Grid>
+          <Grid item>
+            <MetricTile name={"BE Build Time"} />
+          </Grid>
+          <Grid item>
+            <MetricTile name={"FE Build Time"} />
+          </Grid>
+          <Grid item>
+            <MetricTile name={"Monthly Azure Cost"} />
+          </Grid>
         </Grid>
-        <Grid item>
-          <MetricTile name={"BE Build Time"} />
-        </Grid>
-        <Grid item>
-          <MetricTile name={"FE Build Time"} />
-        </Grid>
-        <Grid item>
-          <MetricTile name={"Monthly Azure Cost"} />
-        </Grid>
-      </Grid>
-    </div>
-  );
+      </div>
+    );
+  }
 }
+
+export default Dashboard;
---
+++ frontend/src/config.js
@@ -0,0 +1,5 @@
+export default {
+  api: {
+    URL: "https://localhost:44360"
+  }
+};
